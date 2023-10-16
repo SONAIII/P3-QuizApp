2cdf55ba2198c6cbe93d1e070bf18f7631731ea6
SONAI <mareg200552@gmail.com>
2023-10-16 13:23:33 +0300

    Done

--- scripts.js
+++ scripts.js
@@ -20,8 +20,8 @@ const questions = [
 ];
 
 const questionElement = document.getElementById("question");
-const answerBtn = document.getElementById("answer-buttons");
-const nextBtn = document.getElementsByClassName("next");
+const answerBtns = document.getElementById("answer-buttons");
+const nextBtn = document.getElementById("next");
 
 let score = 0;
 let currentQuestionIndex = 0;
@@ -34,16 +34,68 @@ const startQuiz = () => {
   showQuestion();
 };
 
+const selectAnswer = (e) => {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.status === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerBtns.childNodes).forEach((btn) => {
+    if (btn.dataset.status === "true") {
+      btn.classList.add("correct");
+    }
+    btn.disabled = true;
+  });
+  nextBtn.style.display = "block";
+};
+
 const showQuestion = () => {
+  resetState();
   let currentQuestion = questions[currentQuestionIndex];
   let questionNumber = currentQuestionIndex + 1;
   questionElement.innerHTML = `${questionNumber}. ${currentQuestion.question}`;
+
   currentQuestion.answers.forEach((answer) => {
+    console.log(answer.text);
     const button = document.createElement("button");
-    button.innerHtml = answer.text;
+    button.innerHTML = answer.text;
     button.classList.add("btn");
-    answerBtn.appendChild(button);
+    answerBtns.appendChild(button);
+    if (answer.status) {
+      button.dataset.status = answer.status;
+    }
+    button.addEventListener("click", selectAnswer);
   });
 };
 
+const resetState = () => {
+  nextBtn.style.display = "none";
+  while (answerBtns.firstChild) {
+    answerBtns.removeChild(answerBtns.firstChild);
+  }
+};
+
 startQuiz();
+
+nextBtn.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+      showQuestion();
+    } else {
+      showScore();
+    }
+  } else {
+    startQuiz();
+  }
+});
+
+const showScore = () => {
+  resetState();
+  questionElement.innerHTML = `Your score is ${score}/${questions.length}`;
+  nextBtn.innerHTML = "Play again";
+  nextBtn.style.display = "block";
+};
